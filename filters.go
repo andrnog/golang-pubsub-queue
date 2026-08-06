f5a9ec8d43c510dc07192e7446a4506fc45e8ca8
Голобурдо Андрей Антонович <goloburdoaa@MacBook-Air-Goloburdo.local>
2026-08-06 14:48:35 +0300

    zeroFilter fix

--- filters.go
+++ filters.go
@@ -3,6 +3,6 @@ package pubsubqueue
 // Filter — предикат, применяемый перед публикацией. Возвращает true, если сообщение должно пройти.
 type Filter func(m Message) bool
 
-func zeroFilter(m Message) bool { return m.Value == 0 }
+func zeroFilter(m Message) bool { return m.Value != 0 }
 func evenFilter(m Message) bool { return m.Value%2 == 0 }
 func oddFilter(m Message) bool  { return m.Value%2 != 0 }
